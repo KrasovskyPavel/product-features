dca56d9fd0166419a21a6ddafa8781235a4efc06
Pavel Krasovsky <frostcat2002@gmail.com>
2024-06-20 20:41:31 +0300

    refact: architecture to fsd

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
+      <button onClick={toggleTheme}>Toggle</button>
+    </div>
+  );
+};
+
+export default App;
