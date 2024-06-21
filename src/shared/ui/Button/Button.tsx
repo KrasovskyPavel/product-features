e881eaaf7156b979ce9220f2a5824a35d708df1a
Pavel Krasovsky <frostcat2002@gmail.com>
2024-06-21 10:00:28 +0300

    add: ThemeSwitcher icon

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Button.module.scss";
+import React, { ButtonHTMLAttributes } from "react";
+
+export enum ThemeButton {
+  CLEAR = "clear",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
+}
+
+export const Button: React.FC<ButtonProps> = (props) => {
+  const { className, children, theme, ...otherProps } = props;
+
+  return (
+    <button
+      className={classNames(cls.Button, {}, [cls[theme], className])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
