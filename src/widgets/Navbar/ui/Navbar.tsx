07ddc623d58b693db94a53e5144a2fb12a1a0044
Pavel Krasovsky <frostcat2002@gmail.com>
2024-06-24 16:09:11 +0300

    add: plugin i18next-extract

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import AppLink, { AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { useTranslation } from "react-i18next";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -7,14 +8,15 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation("translation");
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>
         <AppLink to={"/"} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
-          Главная
+          {t("Главная")}
         </AppLink>
         <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
-          О сайте
+          {t("О сайте")}
         </AppLink>
       </div>
     </div>
